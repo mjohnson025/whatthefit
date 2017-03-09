a84695e939ed013b9b3513da14ff68cbfacdb249
Qiyu Zhao <zhaoqiyu@Qiyus-MacBook-Pro.local>
2017-03-09 01:42:27 -0600

    Retrive exercise lkup table

---
+++ module/dbhelper.py
@@ -0,0 +1,48 @@
+import pymysql.cursors
+
+class DBhelper(object):
+	def __init__(self):
+		self.connection = None
+
+	def __openConnection(self):
+		self.connection = pymysql.connect(host='localhost',
+																 user='allen',
+																 password='243546cc',
+																 db='wtf',
+																 charset='utf8mb4',
+																 cursorclass=pymysql.cursors.DictCursor)
+
+
+	def insertWorkout(self, user, exerciseId, exerciseCount):
+		try:
+			self.__openConnection()
+			with self.connection.cursor() as cursor:
+				# Create a new record
+				sql = "INSERT INTO wtf_workouts (add_user, exercise_id, exercise_count) VALUES (%s, %i, %i)"
+				cursor.execute(sql, (user, exercise_id, exercise_count))
+				self.connection.commit()
+		except Exception as e:
+			raise e
+		finally:
+			if self.connection:
+				self.connection.close()
+
+	def getExerciseLookup(self):
+
+		try:
+			self.__openConnection()
+			with self.connection.cursor() as cursor:
+				# Create a new record
+				sql =	( "SELECT w.id, w.name, m.name muscle_group, e.description effort, x.description effort_multiplier "
+								"FROM wtf_exercises w "
+								"LEFT JOIN (wtf_MuscleGroup m, wtf_effort_lkup e, wtf_effort_multiplier_lkup x) "
+								"ON (w.MuscleGroup=m.id AND w.effort=e.id AND w.effort_multiplier=x.id)")
+
+				cursor.execute(sql, (user, exercise_id, exercise_count))
+				result = cursor.fetchall()
+				return result
+		except Exception as e:
+			raise e
+		finally:
+			if self.connection:
+				self.connection.close()
--- startup.py
+++ startup.py
@@ -1,12 +1,18 @@
 from flask import Flask, url_for
 from flask import render_template
+from module.dbhelper import DBhelper
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
+@app.route('/tracker')
+def tracker():
+	helper = DBhelper()
+	exerciseLkupTable = helper.getExerciseLookup()
+	return render_template('tracker.html', exerciseLkupTable=exerciseLkupTable)
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+		application.run(host='0.0.0.0')
