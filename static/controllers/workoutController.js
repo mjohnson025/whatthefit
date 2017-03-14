63a79a0a0bad128ef2b0e259a80ad6aa1ab24941
Qiyu Zhao <zhaoqiyu@Qiyus-MacBook-Pro.local>
2017-03-13 21:18:29 -0500

    Pull table from mysql

---
+++ static/controllers/workoutController.js
@@ -0,0 +1,11 @@
+angular.module('whatthefit', []);
+
+angular.module('whatthefit')
+    .controller('workoutController', workoutController);
+
+workoutController.$inject = ['$scope', '$http'];
+
+function workoutController($scope, $http) {
+
+
+}
