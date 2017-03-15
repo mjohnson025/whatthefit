4fc352a321222a9576cf8fdcf751c760a0475228
Allen Zhao <zhaoqiyu1992@gmail.com>
2017-03-15 04:48:14 +0000

    ??????

--- startup.py
+++ startup.py
@@ -1,6 +1,8 @@
 from flask import Flask, url_for
 from flask import render_template
 from module.dbhelper import DBhelper
+import json
+
 
 app = Flask(__name__)
 
@@ -16,7 +18,7 @@ def tracker():
 		return render_template('tracker.html', exerciseLkupTable=exerciseLkupTable)
 	except Exception as e:
 		exerciseLkupTable=[1,1,2,3,4]
-		return render_template('tracker.html', exerciseLkupTable=exerciseLkupTable)
+		return render_template('tracker.html', exerciseLkupTable=json.dumps(exerciseLkupTable))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
